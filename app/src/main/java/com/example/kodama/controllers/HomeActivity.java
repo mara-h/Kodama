df1a1264ea68e0b7407da4b0e40e78c794425767
Mara <marahusar@yahoo.com>
2021-03-27 22:40:05 +0200

    made buttons usable

--- app/src/main/java/com/example/kodama/controllers/HomeActivity.java
+++ app/src/main/java/com/example/kodama/controllers/HomeActivity.java
@@ -1,13 +1,32 @@
 package com.example.kodama.controllers;
 
+import android.Manifest;
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Environment;
+import android.os.StrictMode;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ImageButton;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
 import android.os.Bundle;
 import com.example.kodama.R;
 
+import java.io.File;
+import java.io.IOException;
+
 public class HomeActivity extends AppCompatActivity {
+    /*
+    public static int index =0;
+    public final String directory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES) + "/";
+    //in tutorial aici intra in files la android sdk in telefon la pictures si o facut un folder nou "myCamera" si la randul de mai sus in ghilimele pun "/myCamera/"
+   */
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +38,7 @@ public class HomeActivity extends AppCompatActivity {
         ImageButton helpButton = (ImageButton) findViewById(R.id.helpButton);
         ImageButton aboutUsButton = (ImageButton) findViewById(R.id.aboutUsButton);
         ImageButton homeButton = (ImageButton) findViewById(R.id.homeButton);
+        ImageButton cameraButton = (ImageButton) findViewById(R.id.buttonCamera);
 
         homeButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,6 +68,41 @@ public class HomeActivity extends AppCompatActivity {
             }
         });
 
+        /*
+        ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, PackageManager.PERMISSION_GRANTED);
+        StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+        StrictMode.setVmPolicy(builder.build());
+         */
+    }
+
+
+    /*
+    public void takePicture(View view) {
+        index++;
+        String file = directory + index + ".jpg";
+        File newFile = new File(file);
+        try {
+            newFile.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Uri outputFileUri = Uri.fromFile(newFile);
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT,outputFileUri);
+        startActivity(cameraIntent);
+    }
+
+     */
+
+    public void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        try {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        } catch (ActivityNotFoundException e) {
+            //display error state to the user
+        }
 
     }
 }
