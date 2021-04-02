b3cf9a823e4a498195795c84df8af3fcd6ae7fac
OanaVasilescu <vasilescu.oana28@gmail.com>
2021-04-03 00:14:21 +0300

    added the model

--- app/src/main/java/com/example/kodama/controllers/AboutUsActivity.java
+++ app/src/main/java/com/example/kodama/controllers/AboutUsActivity.java
@@ -4,7 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+
 import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.kodama.R;
 
 public class AboutUsActivity extends AppCompatActivity {
