1b39367f7475abccb436f515e699c6f1e3ace73a
Mara Husar <mara.husar@gmail.com>
2021-05-09 16:48:14 +0300

    db

---
+++ app/src/main/java/com/example/kodama/view/PlantPageActivity.java
@@ -0,0 +1,13 @@
+package com.example.kodama.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PlantPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
